6685d1b09de27a1980e1ab444c9a2fedb8b785f8
scofflawgames <48799779+scofflawgames@users.noreply.github.com>
2019-09-17 12:52:38 -0400

    Fuck Unity
    Arrays out of bounds wtf?

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -12,16 +12,21 @@ public class RoomSpawner : MonoBehaviour
     // 4 --> need right door
 
 
-    private RoomTemplates templates;
+    public RoomTemplates templates;
     private int rand;
     public bool spawned = false;
 
     public float waitTime = 4f;
 
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
     void Start()
     {
         Destroy(gameObject, waitTime);
-        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        //templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         Invoke("Spawn", 0.1f);
     }
 
@@ -33,26 +38,26 @@ public class RoomSpawner : MonoBehaviour
             if (openingDirection == 1)
             {
                 // Need to spawn a room with a BOTTOM door.
-                rand = Random.Range(0, templates.bottomRooms.Length);
-                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                int rand = 0; //Random.Range(0, bottomRooms.Length);
+                Instantiate(bottomRooms[rand], transform.position, bottomRooms[rand].transform.rotation);
             }
             else if (openingDirection == 2)
             {
                 // Need to spawn a room with a TOP door.
-                rand = Random.Range(0, templates.topRooms.Length);
-                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                int rand = 0; // Random.Range(0, topRooms.Length);
+                Instantiate(topRooms[rand], transform.position, topRooms[rand].transform.rotation);
             }
             else if (openingDirection == 3)
             {
                 // Need to spawn a room with a LEFT door.
-                rand = Random.Range(0, templates.leftRooms.Length);
-                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                int rand = 0; //Random.Range(0, leftRooms.Length);
+                Instantiate(leftRooms[rand], transform.position, leftRooms[rand].transform.rotation);
             }
             else if (openingDirection == 4)
             {
                 // Need to spawn a room with a RIGHT door.
-                rand = Random.Range(0, templates.rightRooms.Length);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                int rand = 0; //Random.Range(0, rightRooms.Length);
+                Instantiate(rightRooms[1], transform.position, rightRooms[rand].transform.rotation);
             }
             spawned = true;
         }
@@ -64,7 +69,7 @@ public class RoomSpawner : MonoBehaviour
         {
             if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
             {
-                //Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
                 //Destroy(gameObject);
             }
             spawned = true;
